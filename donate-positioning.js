862dd9d3ce6a8073b2e4628696db2363d7e468f6
Kim Nguyen <design@kimisgold.com>
2020-03-20 10:21:03 -0400

    Specify button margins, define donation url.

--- donate-positioning.js
+++ donate-positioning.js
@@ -1,8 +1,9 @@
 (function($) {
   $(document).ready(function() {
     var bannerText = 'Support 25 years of building better yesterdays and the production of sites like this.';
+    var donationUrl = 'https://securemason.gmu.edu/s/1564/GID2/16/19-giving.aspx?sid=1564&gid=2&pgid=651&cid=1709&bledit=1&sort=1&dids=318.534.535.176.170&appealcode=IHM02';
 
-    var buttonHtml = '<a href="#" class="donate-button"><img title="Donate to RRCHNM" src="https://rrchnm.org/donate/donate-button-red.svg"></a>';
+    var buttonHtml = '<a href="' + donationUrl + '" class="donate-button"><img title="Donate to RRCHNM" src="https://rrchnm.org/donate/donate-button-red.svg"></a>';
     var bannerHtml = '<div id="support-rrchnm-banner">' + bannerText + buttonHtml + '</div>';
     var banner = $(bannerHtml);
     
